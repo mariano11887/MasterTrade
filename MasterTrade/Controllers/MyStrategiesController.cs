a9f508bafd232e103eb9668c144519393fd2ee68
mariano11887 <Mariano.Jorreto@Alumnos.uai.edu.ar>
2023-06-19 11:58:49 -0300

    Nueva estrategia paso 5

--- MasterTrade/Controllers/MyStrategiesController.cs
+++ MasterTrade/Controllers/MyStrategiesController.cs
@@ -71,5 +71,34 @@ namespace MasterTrade.Controllers
             };
             return View(model);
         }
+
+        public ActionResult NewStep5()
+        {
+            NewStrategyModel model = new NewStrategyModel()
+            {
+                AllExecutionMoments = new List<SelectListItem>
+                {
+                    new SelectListItem { Value = "1", Text = "Cierre de la vela" },
+                    new SelectListItem { Value = "2", Text = "Apertura de la vela" }
+                },
+                StrategyIndicators = new List<SelectListItem>
+                {
+                    new SelectListItem { Value = "1", Text = "Media móvil (5)" },
+                    new SelectListItem { Value = "2", Text = "Media móvil (10)" },
+                    new SelectListItem { Value = "3", Text = "Media móvil (20)" }
+                },
+                MovingAverageValues = new List<SelectListItem>
+                {
+                    new SelectListItem { Value = "1", Text = "Valor" }
+                },
+                AllConditions = new List<SelectListItem>
+                {
+                    new SelectListItem { Value = "1", Text = "mayor que" },
+                    new SelectListItem { Value = "2", Text = "igual a" },
+                    new SelectListItem { Value = "3", Text = "menor que" }
+                }
+            };
+            return View(model);
+        }
     }
 }
