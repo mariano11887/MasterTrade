5475ad1376c6dd2f298c96693680fdf364ea8941
mariano11887 <Mariano.Jorreto@Alumnos.uai.edu.ar>
2023-07-02 19:02:48 -0300

    Backtesting.

---
+++ MasterTrade/Controllers/BacktestingController.cs
@@ -0,0 +1,38 @@
+﻿using MasterTrade.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace MasterTrade.Controllers
+{
+    public class BacktestingController : Controller
+    {
+        // GET: Backtesting
+        public ActionResult Index()
+        {
+            BacktestingModel model = new BacktestingModel
+            {
+                AllStrategies = new List<SelectListItem>
+                {
+                    new SelectListItem() { Text = "Prueba", Value = "1" }
+                },
+                AllCryptoPairs = new List<SelectListItem>
+                {
+                    new SelectListItem() { Text = "BTC/USDT", Value = "1" },
+                    new SelectListItem() { Text = "ETH/USDT", Value = "2" }
+                },
+                AllTemporalities = new List<SelectListItem>
+                {
+                    new SelectListItem() { Text = "5 min", Value = "1" },
+                    new SelectListItem() { Text = "1 hora", Value = "2" }
+                }
+            };
+
+            return View(model);
+        }
+
+        public ActionResult Results()
+        {
+            return View();
+        }
+    }
+}
---
+++ MasterTrade/Models/BacktestingModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Web.Mvc;
+
+namespace MasterTrade.Models
+{
+    public class BacktestingModel
+    {
+        [DisplayName("Estrategia")]
+        public int StrategyId { get; set; }
+
+        [DisplayName("Par de criptomonedas")]
+        public int CryptoPairId { get; set; }
+
+        [DisplayName("Fecha desde")]
+        public DateTime DateFrom { get; set; }
+
+        [DisplayName("Fecha hasta")]
+        public DateTime DateTo { get; set; }
+
+        [DisplayName("Temporalidad")]
+        public int TemporalityId { get; set; }
+
+        public List<SelectListItem> AllStrategies { get; set; }
+        public List<SelectListItem> AllCryptoPairs { get; set; }
+        public List<SelectListItem> AllTemporalities { get; set; }
+    }
+}
