71f07fbd15a0755de466eb65f07c3f21256e4e78
mariano11887 <Mariano.Jorreto@Alumnos.uai.edu.ar>
2023-07-09 17:35:16 -0300

    Backtesting con rangos

---
+++ MasterTrade/Controllers/BacktestingWithRangesController.cs
@@ -0,0 +1,47 @@
+﻿using MasterTrade.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace MasterTrade.Controllers
+{
+    public class BacktestingWithRangesController : Controller
+    {
+        public ActionResult Step1()
+        {
+            BacktestingModel model = new BacktestingModel
+            {
+                AllStrategies = new List<SelectListItem>
+                {
+                    new SelectListItem() { Text = "Prueba", Value = "1" }
+                },
+                AllCryptoPairs = new List<SelectListItem>
+                {
+                    new SelectListItem() { Text = "BTC/USDT", Value = "1" },
+                    new SelectListItem() { Text = "ETH/USDT", Value = "2" }
+                },
+                AllTemporalities = new List<SelectListItem>
+                {
+                    new SelectListItem() { Text = "5 min", Value = "1" },
+                    new SelectListItem() { Text = "1 hora", Value = "2" }
+                }
+            };
+
+            return View(model);
+        }
+
+        public ActionResult Step2()
+        {
+            return View();
+        }
+
+        public ActionResult Confirmation()
+        {
+            return View();
+        }
+
+        public ActionResult Results()
+        {
+            return View();
+        }
+    }
+}
---
+++ MasterTrade/Models/BacktestingWithRangesModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace MasterTrade.Models
+{
+    public class BacktestingWithRangesModel
+    {
+        [DisplayName("Valor mínimo")]
+        public double MinValue { get; set; }
+
+        [DisplayName("Valor máximo")]
+        public double MaxValue { get; set; }
+
+        [DisplayName("Incremento")]
+        public double Increment { get; set; }
+    }
+}
